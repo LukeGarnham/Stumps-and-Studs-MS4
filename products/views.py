d7270c7255ed9ccad40ec0a4efa446ff5497ecb8
LukeGarnham <lukegarnham@msn.com>
2021-04-18 17:33:39 +0000

    Add sort functionality to Products page..

--- products/views.py
+++ products/views.py
@@ -11,8 +11,20 @@ def all_products(request):
     query = None
     categories = None
     sports = None
+    sort = None
+    direction = None
 
     if request.GET:
+        if 'sort' in request.GET:
+            sort = request.GET['sort']
+            sortkey = sort
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
@@ -33,11 +45,14 @@ def all_products(request):
             queries = Q(name__icontains=query) | Q(desc__icontains=query)
             products = products.filter(queries)
 
+    current_sorting = f'{sort}_{direction}'
+
     context = {
         'products': products,
         'search_term': query,
         'current_categories': categories,
         'current_sports': sports,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'products/products.html', context)
