420b8470786a56ba5329893daa15e31339542b36
LukeGarnham <lukegarnham@msn.com>
2021-04-24 12:09:30 +0000

    Change sort on Products page from unordered list to a select box and added JavaScript to PostloadJS to update the sort and direction.

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
+from django.db.models.functions import Lower
 from .models import Product, Category, Sport
 
 
@@ -18,6 +19,11 @@ def all_products(request):
         if 'sort' in request.GET:
             sort = request.GET['sort']
             sortkey = sort
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+            if sortkey == 'category':
+                sortkey = 'category__name'
 
             if 'direction' in request.GET:
                 direction = request.GET['direction']
