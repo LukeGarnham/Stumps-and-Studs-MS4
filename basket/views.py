44a02ca54ade2c2e39ecd86a5c2213394de60669
LukeGarnham <lukegarnham@msn.com>
2021-04-25 09:54:37 +0000

    Add basic add to basket functionality - product id and quantity.

--- basket/views.py
+++ basket/views.py
@@ -1,7 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def view_basket(request):
     """A view to return the basket page"""
 
     return render(request, 'basket/basket.html')
+
+
+def add_to_basket(request, item_id):
+    """ Add a quantity of the product to the basket """
+
+    qty = int(request.POST.get('qty'))
+    redirect_url = request.POST.get('redirect_url')
+    basket = request.session.get('basket', {})
+
+    if item_id in list(basket.keys()):
+        basket[item_id] += qty
+    else:
+        basket[item_id] = qty
+
+    request.session['basket'] = basket
+    print(request.session['basket'])
+    return redirect(redirect_url)
