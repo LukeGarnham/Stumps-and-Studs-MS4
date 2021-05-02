dcb5189bc9f637237545fdfecf7db9a2ffe9dbfa
LukeGarnham <lukegarnham@msn.com>
2021-05-02 14:59:13 +0000

    Create forms.py in basket app and pass in fields from the Order model.  Customise placeholders.

---
+++ checkout/forms.py
@@ -0,0 +1,41 @@
+from django import forms
+from .models import Order
+
+
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = ('full_name', 'email', 'phone_number',
+                  'street_address1', 'street_address2',
+                  'town_or_city', 'county', 'postcode', 'country',)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Create a dictionary of placeholders where:
+        # key = field, value = placeholder.
+        placeholders = {
+            'full_name': 'Full Name',
+            'email': 'Email Address',
+            'phone_number': 'Phone Number',
+            'street_address1': 'Street Address 1',
+            'street_address2': 'Street Address 2',
+            'town_or_city': 'Town/City',
+            'county': 'County',
+            'postcode': 'Postcode',
+            'country': 'Country',
+        }
+
+        # Set the autofocus to the full name field
+        self.fields['full_name'].widget.attrs['autofocus'] = True
+
+        # Loop through all fields.
+        for field in self.fields:
+            # If the field is required, add asterix to placeholder.
+            if self.fields[field].required:
+                placeholder = f'{placeholders[field]} *'
+            else:
+                placeholder = f'{placeholders[field]}'
+            # Set the placeholder attribute to the placeholder created above.
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'stripe-style-input'
+            self.fields[field].label = False
