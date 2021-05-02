286276ea5e6b001f5bb5764ac41d44b9012acefa
LukeGarnham <lukegarnham@msn.com>
2021-05-02 14:44:26 +0000

    Amend basket admin view in the basket/admin.py file.

--- checkout/models.py
+++ checkout/models.py
@@ -9,17 +9,16 @@ from products.models import Product
 
 class Order(models.Model):
     order_number = models.CharField(max_length=32, null=False, editable=False)
+    order_date = models.DateTimeField(auto_now_add=True)
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
     street_address1 = models.CharField(max_length=80, null=False, blank=False)
-    street_address1 = models.CharField(
-        max_length=80, null=True, blank=True)
+    street_address2 = models.CharField(max_length=80, null=True, blank=True)
     town_or_city = models.CharField(max_length=40, null=False, blank=False)
     county = models.CharField(max_length=80, null=True, blank=True)
     postcode = models.CharField(max_length=20, null=True, blank=True)
     country = models.CharField(max_length=40, null=False, blank=False)
-    order_date = models.DateTimeField(auto_now_add=True)
     delivery_cost = models.DecimalField(
         max_digits=6, decimal_places=2, null=False, default=0)
     order_total = models.DecimalField(
@@ -50,6 +49,7 @@ class Order(models.Model):
     def __str__(self):
         return self.order_number
 
+
 class OrderLineItem(models.Model):
     order = models.ForeignKey(
         Order, null=False, blank=False,
@@ -63,7 +63,7 @@ class OrderLineItem(models.Model):
     product_gender = models.CharField(
         max_length=6, null=True, blank=True)  # male, female
     qty = models.IntegerField(null=False, blank=False, default=0)
-    line_item_total = models.DecimalField(
+    lineitem_total = models.DecimalField(
         max_digits=6, decimal_places=2,
         null=False, blank=False, editable=False)
 
