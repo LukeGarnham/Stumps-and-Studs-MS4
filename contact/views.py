b4eb6909e85ca45d0eceb9752c80279c4a076116
LukeGarnham <lukegarnham@msn.com>
2021-05-14 20:27:11 +0000

    Update Contact view so users receive an email when submitting form.

--- contact/views.py
+++ contact/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.contrib import messages
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.conf import settings
 
 from .forms import ContactForm
 
@@ -10,11 +13,31 @@ def contact(request):
     if request.method == 'POST':
         form = ContactForm(request.POST)
         if form.is_valid():
+            contact_message = {
+                'full_name': request.POST['full_name'],
+                'email': request.POST['email'],
+                'message': request.POST['message'],
+            }
             form.save()
-            print('Succes')
+
+            # Send the user an email acknowledging their message.
+            cust_email = request.POST['email'],
+            subject = render_to_string(
+                'contact/confirmation_emails/confirmation_email_subject.txt',
+                {'contact_message': contact_message})
+            body = render_to_string(
+                'contact/confirmation_emails/confirmation_email_body.txt',
+                {'contact_message': contact_message,
+                 'contact_email': settings.DEFAULT_FROM_EMAIL})
+            send_mail(
+                subject,
+                body,
+                settings.DEFAULT_FROM_EMAIL,
+                [cust_email]
+            )
+
             messages.success(request, 'Thank you for your message.  \
                 Someone will respond via email soon.')
-            print('Succes - after message')
         else:
             messages.error(request, 'Message failed.  Please ensure the \
                 form is valid.')
