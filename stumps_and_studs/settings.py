3ea8b2b467fb32a66e0dd79e346864e003461ac2
LukeGarnham <lukegarnham@msn.com>
2021-05-15 17:29:50 +0000

    Temporarily connect to Postgres database to migrate models and import db.json into it.

--- stumps_and_studs/settings.py
+++ stumps_and_studs/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -131,6 +132,10 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+# DATABASES = {
+#     'default': dj_database_url.parse()
+# }
 
 
 # Password validation
