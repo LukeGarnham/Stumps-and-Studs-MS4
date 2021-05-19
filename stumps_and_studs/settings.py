5be2e889e3e0bbb10e27f1b7a79c1340ef109c6c
LukeGarnham <lukegarnham@msn.com>
2021-05-19 19:07:52 +0000

    Adjust email settings in global settings file as not currnetly working in deployed environment.

--- stumps_and_studs/settings.py
+++ stumps_and_studs/settings.py
@@ -220,9 +220,9 @@ if 'DEVELOPMENT' in os.environ:
     DEFAULT_FROM_EMAIL = 'contactus@stumpsandstuds.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_USE_TLMS = True
-    EMAIL_PORT = 587
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    EMAIL_PORT = 587
+    EMAIL_USE_TLS = True
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
