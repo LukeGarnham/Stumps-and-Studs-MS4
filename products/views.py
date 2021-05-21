cce67f17dccf3b9052bb213d8e2b4c204c126c37
LukeGarnham <lukegarnham@msn.com>
2021-05-21 09:38:21 +0000

    Fix bug which meant the sort by sport option on the Products page wasn't working correctly.

--- products/views.py
+++ products/views.py
@@ -24,6 +24,8 @@ def all_products(request):
                 products = products.annotate(lower_name=Lower('name'))
             if sortkey == 'category':
                 sortkey = 'category__name'
+            if sortkey == 'sport':
+                sortkey = 'sport__name'
 
             if 'direction' in request.GET:
                 direction = request.GET['direction']
