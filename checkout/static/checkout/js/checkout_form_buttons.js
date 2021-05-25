1fe795bcc0616fbd11c352fe68aec0d7a93fa8d3
LukeGarnham <lukegarnham@msn.com>
2021-05-25 17:42:12 +0000

    Refactor and improve the JS which controls the checkout form buttons in the Checkout page.

--- checkout/static/checkout/js/checkout_form_buttons.js
+++ checkout/static/checkout/js/checkout_form_buttons.js
@@ -1,7 +1,5 @@
-// Script to handle checkout form next and previous buttons.
-
-// Check the required input fields on the personal tab are complete when there is an input on any one of them and enable/disable delivery.
-$('#personal :input[required]').on('input', function() {
+// Check the required input fields on the personal tab all have values.  If so, enable delivery tab and next button.  Else disable both.
+function checkPersonalDetails() {
     if (($('#id_full_name').val() != "") && ($('#id_email').val() != "") && ($('#id_phone_number').val() != "")) {
         $('#personal-to-delivery').prop('disabled', false);
         $('#delivery-tab').removeClass('disabled');
@@ -11,54 +9,51 @@ $('#personal :input[required]').on('input', function() {
         $('#delivery-to-pay').prop('disabled', true);
         $('#pay-tab').addClass('disabled');
     }
+}
+
+// Check the required input fields on the delivery tab all have values.  If so, enable pay tab and next button.  Else disable both.
+function checkDeliveryDetails() {
+    if (($('#id_street_address1').val() != "") && ($('#id_town_or_city').val() != "") && ($('#id_country').val() != "")) {
+        $('#delivery-to-pay').prop('disabled', false);
+        $('#pay-tab').removeClass('disabled');
+    } else {
+        $('#delivery-to-pay').prop('disabled', true);
+        $('#pay-tab').addClass('disabled');
+    }
+}
+
+// Call checkPersonalDetails whenever there is an input on any of the required fields on the Personal tab.
+$('#personal :input[required]').on('input', function() {
+    checkPersonalDetails();
 });
 
-// Move to delivery tab when next button on personal tab is clicked.
-$('#personal-to-delivery').click(function(e) {
+// Call checkDeliveryDetails whenever there is an input on any of the required fields on the Delivery tab.
+$('#delivery :input[required]').on('input', function() {
+    checkDeliveryDetails();
+});
+
+// Move to delivery tab when next button on personal tab or delivery tab button is clicked and call checkDeliveryDetails.
+$('#personal-to-delivery, #delivery-tab').click(function(e) {
     e.preventDefault();
     $('#personal').removeClass("active show");
     $('#personal-tab').removeClass("active").attr("aria-selected", "false");
     $('#delivery').addClass("active show");
     $('#delivery-tab').addClass("active").attr("aria-selected", "true");
-    if (($('#id_street_address1').val() != "") && ($('#id_town_or_city').val() != "") && ($('#id_country').val() != "")) {
-        $('#delivery-to-pay').prop('disabled', false);
-        $('#pay-tab').removeClass('disabled');
-    }
+    checkDeliveryDetails();
 });
 
-// Move to personal tab when prev button on delivery tab is clicked.
-$('#delivery-to-personal').click(function(e) {
+// Move to personal tab when prev button on delivery tab is clicked and call checkPersonalDetails.
+$('#delivery-to-personal, #personal-tab').click(function(e) {
     e.preventDefault();
     $('#delivery').removeClass("active show");
     $('#delivery-tab').removeClass("active").attr("aria-selected", "false");
     $('#personal').addClass("active show");
     $('#personal-tab').addClass("active").attr("aria-selected", "true");
-});
-
-// Check the required input fields on the delivery tab are complete when page loads and enable/disable pay.
-$('#delivery :input[required]').on('load', function() {
-    if (($('#id_street_address1').val() != "") && ($('#id_town_or_city').val() != "") && ($('#id_country').val() != "")) {
-        $('#delivery-to-pay').prop('disabled', false);
-        $('#pay-tab').removeClass('disabled');
-    } else {
-        $('#delivery-to-pay').prop('disabled', true);
-        $('#pay-tab').addClass('disabled');
-    }
-});
-
-// Check the required input fields on the delivery tab are complete when there is an input on any one of them and enable/disable pay.
-$('#delivery :input[required]').on('input', function() {
-    if (($('#id_street_address1').val() != "") && ($('#id_town_or_city').val() != "") && ($('#id_country').val() != "")) {
-        $('#delivery-to-pay').prop('disabled', false);
-        $('#pay-tab').removeClass('disabled');
-    } else {
-        $('#delivery-to-pay').prop('disabled', true);
-        $('#pay-tab').addClass('disabled');
-    }
+    checkPersonalDetails();
 });
 
 // Move to pay tab when next button on delivery tab is clicked.
-$('#delivery-to-pay').click(function(e) {
+$('#delivery-to-pay, #pay-tab').click(function(e) {
     e.preventDefault();
     $('#delivery').removeClass("active show");
     $('#delivery-tab').removeClass("active").attr("aria-selected", "false");
@@ -66,11 +61,12 @@ $('#delivery-to-pay').click(function(e) {
     $('#pay-tab').addClass("active").attr("aria-selected", "true");
 });
 
-// Move to delivery tab when prev button on pay tab is clicked.
-$('#pay-to-delivery').click(function(e) {
+// Move to delivery tab when prev button on pay tab is clicked and call checkDeliveryDetails.
+$('#pay-to-delivery, #delivery-tab').click(function(e) {
     e.preventDefault();
     $('#pay').removeClass("active show");
     $('#pay-tab').removeClass("active").attr("aria-selected", "false");
     $('#delivery').addClass("active show");
     $('#delivery-tab').addClass("active").attr("aria-selected", "true");
+    checkDeliveryDetails();
 });
